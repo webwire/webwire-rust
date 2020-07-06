ad37a7de1d3146ab1cde02dc2ee4aab8c4152617
Michael P. Jung <michael.jung@terreon.de>
2020-07-06 17:26:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Webwire Command-line Interface
+[![Crates.io](https://img.shields.io/crates/v/webwire)](https://crates.io/crates/webwire)
+[![GitHub](https://img.shields.io/github/license/webwire/webwire)](https://github.com/webwire/webwire/blob/master/LICENSE)
+[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/webwire/webwire/Rust)](https://github.com/webwire/webwire/actions)
+
+[![Discord Chat](https://img.shields.io/discord/726922033039933472?label=Discord+Chat&color=%23677bc4&logo=discord&logoColor=white&style=for-the-badge)](https://discord.gg/jjD6aWG)
+
+![webwire logo](https://raw.githubusercontent.com/webwire/webwire-docs/master/docs/logo.svg)
+
+Webwire is a **contract-first API system** which features an
+interface description language a network protocol and
+code generator for both servers and clients.
+
+This repository contains the the library for writing clients and
+servers using the Rust programming language.
+
+To learn more about webwire in general please visit the documentation
+repository [webwire/webwire-docs](https://github.com/webwire/webwire-docs).
+
+# WORK IN PROGRESS
+
+> **Webwire is not ready to use! All the documentation and code in this
+repository is either incomplete or non functional. Don't expect anything
+to work, yet. Right now this repository is just a collection of ideas and
+preliminary implementations.**
---
+++ src/lib.rs
@@ -0,0 +1,92 @@
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use dashmap::DashMap;
+
+
+#[derive(Debug)]
+pub enum ProviderError {
+    ServiceNotFound,
+    MethodNotFound,
+    SerializerError(serde_json::Error),
+    DeserializerError(serde_json::Error),
+    InternalError(Box<dyn std::error::Error>),
+}
+
+#[derive(Debug)]
+pub enum ConsumerError {
+    SerializerError(serde_json::Error),
+    DeserializerError(serde_json::Error),
+    ProviderError(ProviderError), // FIXME
+    /// The remote side disconnected while waiting for a response
+    Disconnected,
+}
+
+impl From<ProviderError> for ConsumerError {
+    fn from(e: ProviderError) -> Self {
+        Self::ProviderError(e)
+    }
+}
+
+pub struct Request {
+    pub service: String,
+    pub method: String,
+    pub data: Vec<u8>,
+}
+
+pub type Response<E> = Result<Vec<u8>, E>;
+
+#[derive(Clone)]
+pub struct ServerConnection {
+    // FIXME
+}
+
+#[async_trait]
+impl Consumer for ServerConnection {
+    async fn call(&self, input: &Request) -> Result<Vec<u8>, ConsumerError> {
+        // FIXME implement
+        unimplemented!();
+    }
+    fn notify(&self, input: &Request) -> Result<(), ConsumerError> {
+        // FIXME implement
+        unimplemented!();
+    }
+}
+
+#[derive(Clone, Default)]
+pub struct Server {
+    inner: Arc<ServerInner>,
+}
+
+#[derive(Default)]
+struct ServerInner {
+    connections: Vec<ServerConnection>,
+    providers: DashMap<String, Box<dyn Provider + Sync + Send>>,
+}
+
+impl Server {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    pub fn provider<P: Provider + Sync + Send + 'static>(&mut self, provider: P) {
+        self.inner
+            .providers
+            .insert(provider.name().to_owned(), Box::new(provider));
+    }
+    pub fn connections(&self) -> std::slice::Iter<ServerConnection> {
+        self.inner.connections.iter()
+    }
+}
+
+#[async_trait]
+pub trait Consumer {
+    async fn call(&self, request: &Request) -> Result<Vec<u8>, ConsumerError>;
+    fn notify(&self, request: &Request) -> Result<(), ConsumerError>;
+}
+
+#[async_trait]
+pub trait Provider {
+    fn name(&self) -> &'static str;
+    async fn call(&self, request: &Request) -> Result<Vec<u8>, ProviderError>;
+    async fn notify(&self, request: &Request) -> Result<(), ProviderError>;
+}
