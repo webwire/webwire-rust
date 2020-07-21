ae06943d35e10a98cb23b51769ce4d68a1eeb47f
Michael P. Jung <michael.jung@terreon.de>
2020-07-21 21:03:37 +0200

    Re-export types at crate root

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,13 @@ pub mod rpc;
 pub mod server;
 pub mod service;
 pub mod utils;
+
+pub use {
+    server::{
+        session::{Session, SessionHandler, DefaultSessionHandler},
+        Server,
+    },
+    service::{
+        Consumer, ConsumerError, Provider, ProviderError, ProviderRegistry, Request, Response,
+    },
+};
