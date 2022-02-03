bdcf47a0d5cccbe4aa556b497839c3957075d9e1
Michael P. Jung <michael.jung@terreon.de>
2022-02-03 01:29:40 +0100

    Fix redis tests

--- tests/test_redis.rs
+++ tests/test_redis.rs
@@ -1,5 +1,7 @@
 #![cfg(feature = "redis")]
 
+use std::time::Duration;
+
 use bytes::Bytes;
 use redis::{Client, ConnectionInfo};
 use tokio::sync::{mpsc, Mutex};
@@ -17,7 +19,6 @@ impl<S: Sync + Send> Provider<S> for FakeService {
         method: &str,
         data: bytes::Bytes,
     ) -> futures::future::BoxFuture<'static, Result<Bytes, webwire::ProviderError>> {
-        println!("Provider being called!");
         assert_eq!(service, "test_service");
         assert_eq!(method, "test_method");
         assert_eq!(data, "test_data");
@@ -35,24 +36,20 @@ impl<S: Sync + Send> NamedProvider<S> for FakeService {
 
 #[tokio::test]
 async fn test_redis() {
-    let connection_info = "redis://127.0.0.1/".parse::<ConnectionInfo>().unwrap();
+    let redis_url = "redis://127.0.0.1";
     let router = Router::new();
     let (tx, mut rx) = mpsc::channel(1);
     router.service(FakeService { tx });
-    let listener = webwire::redis::RedisListener::new(
-        connection_info.clone(),
-        "test_webwire".to_string(),
-        router,
-    )
-    .unwrap();
+    let listener = webwire::redis::RedisListener::new(redis_url, "test_webwire", router).unwrap();
     tokio::spawn(listener.run());
     // FIXME wait for provider to be ready
     // The easiest way to implement this feature would be a signal
     // sent by the listener whenever it is connected to redis and
     // ready to receive messages.
-    let client = Client::open(connection_info).unwrap();
-    let conn = client.get_async_connection().await.unwrap();
-    let publisher = RedisPublisher::new(conn, "test_webwire");
+    tokio::time::sleep(Duration::from_millis(100)).await;
+    let mgr = deadpool_redis::Manager::new(redis_url).unwrap();
+    let pool = deadpool_redis::Pool::builder(mgr).build().unwrap();
+    let publisher = RedisPublisher::new(pool, "test_webwire");
     publisher.notify(
         "test_service",
         "test_method",
